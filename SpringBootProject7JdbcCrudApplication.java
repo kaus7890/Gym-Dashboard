14420830897f9c5f9f1df48725d9573c59ebb073
kaustav yadav <143779491+kaus7890@users.noreply.github.com>
2025-07-19 11:18:46 +0530

    Create SpringBootProject7JdbcCrudApplication.java

---
+++ SpringBootProject7JdbcCrudApplication.java
@@ -0,0 +1,12 @@
+package in.sp.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject7JdbcCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject7JdbcCrudApplication.class, args);
+	}
+}
