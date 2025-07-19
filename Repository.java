69afc1219fc6a7c8810fd7306aafa107609c4b4e
kaustav yadav <143779491+kaus7890@users.noreply.github.com>
2025-07-19 11:31:48 +0530

    Delete Repository.java

--- Repository.java
+++
@@ -1,7 +0,0 @@
-package in.sp.main.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import in.sp.main.entity.ContactMessage;
-
-public interface ContactMessageRepository extends JpaRepository<ContactMessage, Long> {
-}
